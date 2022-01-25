d5d44ef8ea1170f8279c23101fc2d1056474d0da
demiguelmoreno <manuel.demiguelmoreno@gmail.com>
2022-01-25 19:25:58 +0100

    Enable_intranode_visibility

--- GKE vuln.tf
+++ GKE vuln.tf
@@ -20,7 +20,9 @@ resource "google_container_cluster" "workload_cluster" {
       cidr_block = "0.0.0.0/0"
     }
   }
+  enable_intranode_visibility = true
 }
+
 
 resource google_container_node_pool "custom_node_pool" {
   cluster  = google_container_cluster.workload_cluster.name
