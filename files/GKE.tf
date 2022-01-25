527c3518d4713a2e579bd62884d2d788f5015a5c
Manuel De Miguel Moreno <manuel.demiguelmoreno@gmail.com>
2022-01-25 21:52:15 +0100

    Update GKE.tf

--- files/GKE.tf
+++ files/GKE.tf
@@ -22,9 +22,6 @@ resource "google_container_cluster" "workload_cluster" {
   }
 }
 
-
-
-
 resource google_container_node_pool "custom_node_pool" {
   cluster  = google_container_cluster.workload_cluster.name
   location = var.region
